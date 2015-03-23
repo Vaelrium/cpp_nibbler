c9eac77b9a3a481816d135bd2db651685685d072
durand_u <durand_u@epitech.eu>
2015-03-23 14:23:59 +0100

    lol

--- init.cpp
+++ init.cpp
@@ -5,20 +5,20 @@
 // Login   <durand_u@epitech.net>
 // 
 // Started on  Mon Mar 23 11:16:54 2015 Rémi DURAND
-// Last update Mon Mar 23 13:04:59 2015 Rémi DURAND
+// Last update Mon Mar 23 14:16:25 2015 Rémi DURAND
 //
 
 #include "nibbler.hpp"
 
-int		init_nib(int width, int height, std::string lib, void **handler)
+int		init_nib(const int width, const int height, std::string lib, void **handler)
 {
-  if (width <= 4 || height <= 4 || (height * width) < 40)
+  if (width < 4 || width > 50 || height < 4 || height > 50 || (height * width) < 40)
     {
-      std::cout << "Area must at least have 40 blocks" << std::endl;
+      std::cout << "Area must have [40-100] blocks" << std::endl;
       return (-1);
     }
   lib = "./" + lib;
-  *handler = dlopen(lib.c_str(), RTLD_NOW);
+  *handler = dlopen(lib.c_str(), RTLD_LAZY);
   if (*handler == NULL)
     {
       std::cout << dlerror() << std::endl;
--- nibbler.hpp
+++ nibbler.hpp
@@ -5,7 +5,7 @@
 // Login   <durand_u@epitech.net>
 // 
 // Started on  Mon Mar 23 10:41:04 2015 Rémi DURAND
-// Last update Mon Mar 23 12:53:08 2015 Rémi DURAND
+// Last update Mon Mar 23 14:18:39 2015 Rémi DURAND
 //
 
 #ifndef _NIB_H_
@@ -18,6 +18,6 @@
 
 #define BLOCK_SIZE 10
 
-int		init_nib(int width, int height, std::string lib, void **handler);
+int		init_nib(const int width, const int height, std::string lib, void **handler);
 
 #endif /* !_NIB_H_ */
